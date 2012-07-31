bcb40b2ef1a2c857b04fa85af5bc75afd334265b
Darrell Johnson <darrellj@gmail.com>
2012-07-31 13:07:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetraq::Application.config.secret_token = 'a13c3b71f359833f4fa87ead75fc838fa6c067427c3ed4026d4b4c186d67e4661c40cc5dc9f2b16c5149b28beabfa3d41b38b4f8c5b41e9d39934e945f07f19e'
