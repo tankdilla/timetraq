1e18c56f1ca6dd0e2b94cda3478272bad0607650
Darrell Johnson <darrellj@gmail.com>
2012-08-08 15:22:22 -0500

    starting to write model logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User
   field :name, type: String
   #field :_id, type: String, default ->{ name } #set this to have a custom id, prettier url
   field :email, type: String
-  
 
   embeds_many :activities
 
   def day_entries(date=Date.today)
-    #activities.collect{|a| a.entries.collect{|e| e if e.start_time.to_date == date}}.compact.flatten
     activities.collect{|a| a.entries.from_today.entries}.flatten
+  end
+  
+  def day_score(date=Date.today)
+    day_entries.inject(0){|result, entry| result + entry.score}
   end
 end
